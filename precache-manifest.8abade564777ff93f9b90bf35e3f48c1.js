ed19bdc65ba31b048bfadb6c673a985dbcae63a6
EmmanuelBMS <acquabriall2020@gmail.com>
2022-11-16 19:01:44 -0300

    Updates

---
+++ precache-manifest.8abade564777ff93f9b90bf35e3f48c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f389001c5687194418bf7d756f9dc36a",
+    "url": "/starwars-planet-search/index.html"
+  },
+  {
+    "revision": "548ddbcbc8c36fb3085b",
+    "url": "/starwars-planet-search/static/js/2.f0255684.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planet-search/static/js/2.f0255684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600f610ada7258b07545",
+    "url": "/starwars-planet-search/static/js/main.f2436071.chunk.js"
+  },
+  {
+    "revision": "7ae9ac9354f1d33a3553",
+    "url": "/starwars-planet-search/static/js/runtime-main.9d6297ae.js"
+  }
+]);
